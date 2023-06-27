c4e19f8aa25b8756bdcb8f39c980d425d010b6f9
KiosaOleg <117593116+KiosaOleg@users.noreply.github.com>
2023-06-27 22:36:23 +0200

    ыф

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,32 +1,24 @@
 import Modal from 'components/Modal/Modal';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export default class ImageGalleryItem extends Component {
-  state = {
-    modalIsOpen: false,
+export default function ImageGalleryItem({ smallImage, largeImage, tags }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalIsOpen(modalIsOpen => !modalIsOpen);
   };
 
-  toggleModal = () => {
-    this.setState(({ modalIsOpen }) => ({
-      modalIsOpen: !modalIsOpen,
-    }));
-  };
-  render() {
-    const { smallImage, largeImage, tags } = this.props;
-    const { modalIsOpen } = this.state;
-
-    return (
-      <>
-        <li className="ImageGalleryItem" onClick={this.toggleModal}>
-          <img className="ImageGalleryItem-image" src={smallImage} alt={tags} />
-        </li>
-        {modalIsOpen && (
-          <Modal closeModal={this.toggleModal}>
-            <img src={largeImage} alt={tags} />
-            <p className="tags">{tags}</p>
-          </Modal>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <li className="ImageGalleryItem" onClick={toggleModal}>
+        <img className="ImageGalleryItem-image" src={smallImage} alt={tags} />
+      </li>
+      {modalIsOpen && (
+        <Modal closeModal={toggleModal}>
+          <img src={largeImage} alt={tags} />
+          <p className="tags">{tags}</p>
+        </Modal>
+      )}
+    </>
+  );
 }
